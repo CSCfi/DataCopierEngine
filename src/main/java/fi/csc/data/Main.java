24c002fd7ba13ba13e06a3fe46956f72657dc658
Pekka Järveläinen <pj@csc.fi>
2021-08-24 09:40:30 +0300

    org/apache/commons/logging/LogFactory missing error, storing before refactoring, what it should be!

--- src/main/java/fi/csc/data/Main.java
+++ src/main/java/fi/csc/data/Main.java
@@ -1,18 +1,56 @@
 package fi.csc.data;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.URI;
 import software.amazon.awssdk.core.waiters.WaiterResponse;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.*;
 import software.amazon.awssdk.services.s3.waiters.S3Waiter;
+/*import software.amazon.awssdk.auth.AWSCredentials;
+import software.amazon.awssdk.auth.AWSStaticCredentialsProvider;
+import software.amazon.awssdk.auth.BasicAWSCredentials;*/
 
 public class Main {
 
+    /*private static final String FILENAME = "~/.s3cfg";
+    private static final String SECTRETKEY = "secret_key =";
+    private static final String ACCESSKEY = "access_key =";*/
     //software.amazon.awssdk.http.crt.AwsCrtAsyncHttpClient
 
     public static void main(String[] args) {
-        S3Client s3 = S3Client.builder().endpointOverride(URI.create("https://a3s.fi")).build();
-
+        /*String[] keys = lueTiedosto(FILENAME);
+        String accesskey = keys[0];
+        String secretkey = keys[1];
+        AWSCredentials credentials = new BasicAWSCredentials(accesskey, secretkey);*/
+        S3Client s3 = S3Client.builder()
+                .region(Region.of("US"))
+                //.withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .endpointOverride(URI.create("https://a3s.fi")).build();
+        //ListBucketsRequest listBucketsRequest = ListBucketsRequest.builder().build();
+        ListBucketsResponse listBucketsResponse = s3.listBuckets();
+        listBucketsResponse.buckets().stream().forEach(x -> System.out.println(x.name()));
     }
+
+    /*
+    private static String[] lueTiedosto(String filename) {
+
+        String[] keys = new String[2];
+        String inputLine;
+        Boolean eivielälöydetty = true;
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            while ((inputLine = reader.readLine()) != null) {
+                if (inputLine.startsWith(SECTRETKEY))
+                    keys[1] =  inputLine.substring(SECTRETKEY.length()).trim();
+                if (eivielälöydetty && inputLine.startsWith(ACCESSKEY)) {
+                    keys[0] = inputLine.substring(ACCESSKEY.length()).trim();
+                    eivielälöydetty = false;
+                }
+            }
+        }  catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }*/
 }

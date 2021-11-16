45a95e2633fe6d21e115215d9ff26dc98b41b99a
Pekka Järveläinen <pj@csc.fi>
2021-11-16 09:16:27 +0200

    Hello world with rclone

--- src/main/java/fi/csc/data/ApplicationLifecycle.java
+++ src/main/java/fi/csc/data/ApplicationLifecycle.java
@@ -7,8 +7,13 @@ import org.jboss.logging.Logger;
 import javax.inject.Inject;
 import io.agroal.api.AgroalDataSource;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.concurrent.Executors;
+import java.util.function.Consumer;
 
 
 @QuarkusMain
@@ -25,9 +30,15 @@ public class ApplicationLifecycle implements QuarkusApplication {
 
      System.out.println("Hello " + args[1]);
      try  {
-                Connection connection = defaultDataSource.getConnection();
-                log.info(Base.read(connection));
-                connection.close();
+         Connection connection = defaultDataSource.getConnection();
+         log.info(Base.read(connection));
+         Process process = Runtime.getRuntime().exec("/work/rclone --version");
+         StreamGobbler streamGobbler =
+                 new StreamGobbler(process.getInputStream(), System.out::println);
+         Executors.newSingleThreadExecutor().submit(streamGobbler);
+         int exitCode = process.waitFor();
+         assert exitCode == 0;
+         connection.close();
     } catch (SQLException throwables) {
          throwables.printStackTrace();
          return 10;
@@ -36,4 +47,19 @@ public class ApplicationLifecycle implements QuarkusApplication {
     }
 
 
+private static class StreamGobbler implements Runnable {
+    private InputStream inputStream;
+    private Consumer<String> consumer;
+
+    public StreamGobbler(InputStream inputStream, Consumer<String> consumer) {
+        this.inputStream = inputStream;
+        this.consumer = consumer;
+    }
+
+    @Override
+    public void run() {
+        new BufferedReader(new InputStreamReader(inputStream)).lines()
+          .forEach(consumer);
+    }
+}
 }
